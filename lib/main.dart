d9fbc5608d02074b4caa7ddccb0cd1c8cfa3de05
Alexsamurai-code <82030354+Alexsamurai-code@users.noreply.github.com>
2025-01-08 13:34:26 +0200

    three commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'dart:convert';
+import 'dart:io';
 import 'package:http/http.dart' as http;
 
 class Package {
@@ -15,17 +16,28 @@ class Package {
 }
 
 void main() async {
-  final httpPackageUrl = Uri.https('dart.dev', '/f/packages/http.json');
-  final httpPackageResponse = await http.get(httpPackageUrl);
-  if (httpPackageResponse.statusCode != 200) {
-    print('Failed to retrieve the http package!');
+  print('Введіть ім\'я пакета (наприклад, http):');
+  String? packageName = stdin.readLineSync()?.trim();
+
+  if (packageName == null || packageName.isEmpty) {
+    print('Ім\'я пакета не може бути порожнім!');
     return;
   }
-  final json = jsonDecode(httpPackageResponse.body);
+
+  final packageUrl = Uri.https('dart.dev', '/f/packages/$packageName.json');
+  final packageResponse = await http.get(packageUrl);
+
+  if (packageResponse.statusCode != 200) {
+    print('Не вдалося отримати інформацію про пакет $packageName!');
+    return;
+  }
+
+  final json = jsonDecode(packageResponse.body);
   final package = Package(
     json['name'],
     json['latestVersion'],
     description: json['description'],
   );
+
   print(package);
 }
